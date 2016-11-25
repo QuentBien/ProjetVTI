870fb1b5ea1bfed4bc2ccf26959d755b1fb251d2
RAMONI Ted <eagleframe@gmail.com>
2016-11-25 16:04:29 +0100

    formulaire dans new.ejs a integrer en one page

--- routes/films.js
+++ routes/films.js
@@ -44,7 +44,7 @@ router.param('id', function(req, res, next, id) {
         //if it is found we continue on
         } else {
             //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
-            //console.log(blob);
+            console.log(film);
             // once validation is done save the new item in the req
             req.id = id;
             // go to the next thing
@@ -76,7 +76,6 @@ router.route('/')
               if (err) {
                 throw err;
               } else {
-                  console.log(films);
                   //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
                   res.format({
                       //HTML response will render the index.jade file in the views/blobs folder. We are also setting "blobs" to be an accessible variable in our jade view
