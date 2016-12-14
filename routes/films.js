d7729753d6d0b29ca8b8317c7fa93d6245b7ec65
QuentBien <chauvel.quentin.95@gmail.com>
2016-12-14 17:06:54 +0100

    delete dead code

--- routes/films.js
+++ routes/films.js
@@ -1,16 +1,13 @@
 var express = require('express'),
     router = express.Router(),
     mongoose = require('mongoose'),
-    bodyParser = require('body-parser'), //parses information from POST
-    methodOverride = require('method-override'), //used to manipulate POST
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
     path = require('path');
 
-//Any requests to this controller must pass through this 'use' function
-//Copy and pasted from method-override
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
         var method = req.body._method
         delete req.body._method
         return method
@@ -22,12 +19,8 @@ router.get('/new', function (req, res) {
     res.render('films/new', { title: 'Ajouter un film' });
 });
 
-// route middleware to validate :id
 router.param('id', function (req, res, next, id) {
-    //console.log('validating ' + id + ' exists');
-    //find the ID in the Database
     mongoose.model('film').findById(id, function (err, film) {
-        //if it isn't found, we are going to repond with 404
         if (err) {
             console.log(id + ' was not found');
             res.status(404)
@@ -41,51 +34,29 @@ router.param('id', function (req, res, next, id) {
                     res.json({ message: err.status + ' ' + err });
                 }
             });
-            //if it is found we continue on
         } else {
             //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
             //console.log(film);
             // once validation is done save the new item in the req
             req.id = id;
-            // go to the next thing
             next();
         }
     });
 });
 
-/*router.route('/')
-  .get(function(req, res, next) {
-	//var filmController = require('../controller/filmController');
-  var films;
-  mongoose.model('film').find({}, function(err, movies){
-	films = movies;
-  });
-   res.render('films', {
-          tagline: 'All my films',
-          "films": films
-      });
-});*/
-
-//build the REST operations at the base for blobs
-//this will be accessible from http://127.0.0.1:3000/blobs if the default route for / is left unchanged
 router.route('/')
-    //GET all blobs
     .get(function (req, res, next) {
-        //retrieve all blobs from Monogo
         mongoose.model('film').find({}, function (err, films) {
             if (err) {
                 throw err;
             } else {
-                //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
                 res.format({
-                    //HTML response will render the index.jade file in the views/blobs folder. We are also setting "blobs" to be an accessible variable in our jade view
-                    html: function () {
+			html: function () {
                         res.render('films', {
                             tagline: 'All my films',
                             "films": films
                         });
                     },
-                    //JSON response will show all blobs in JSON format
                     json: function () {
                         res.json(films);
                     }
@@ -94,7 +65,6 @@ router.route('/')
         });
     })
     .post(function (req, res) {
-        // Get values from POST request. These can be done through forms or REST calls. These rely on the "name" attributes for forms
         var _id;
         var title = req.body.title;
         var year = req.body.year;
@@ -116,7 +86,6 @@ router.route('/')
             temp = acteur.split(" ");
             actors.push({ last_name: temp[1], first_name: temp[0] });
         }, this);
-        //call the create function for our database
         mongoose.model('film').find({}, function (err, films) {
             if (err) {
                 console.log(err);
@@ -142,18 +111,12 @@ router.route('/')
                         console.log(err);
                         res.send("There was a problem adding the information to the database.");
                     } else {
-                        //Film has been created
                         console.log('POST creating new film: ' + film);
                         res.format({
-                            //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
                             html: function () {
-                                // If it worked, set the header so the address bar doesn't still say /adduser
                                 res.location("films");
-
-                                // And forward to success page
                                 res.redirect("/films");
                             },
-                            //JSON response will show the newly created blob
                             json: function () {
                                 res.json(film);
                             }
@@ -176,16 +139,13 @@ router.route('/search')
             if (err) {
                 throw err;
             } else {
-                //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
                 res.format({
-                    //HTML response will render the index.jade file in the views/blobs folder. We are also setting "blobs" to be an accessible variable in our jade view
                     html: function () {
                         res.render('films', {
                             tagline: 'All my films',
                             "films": films
                         });
                     },
-                    //JSON response will show all blobs in JSON format
                     json: function () {
                         res.json(films);
                     }
@@ -217,7 +177,6 @@ function compare(a, b) {
 
 router.route('/update')
     .post(function (req, res) {
-        // Get values from POST request. These can be done through forms or REST calls. These rely on the "name" attributes for forms
         var _id = req.body._id;
         var title = req.body.title;
         var year = req.body.year;
@@ -239,7 +198,6 @@ router.route('/update')
             temp = acteur.split(" ");
             actors.push({ last_name: temp[1], first_name: temp[0] });
         }, this);
-        //call the create function for our database
         mongoose.model('film').find({}, function (err, films) {
             if (err) {
                 console.log(err);
@@ -260,18 +218,12 @@ router.route('/update')
                         console.log(err);
                         res.send("There was a problem updating the information to the database.");
                     } else {
-                        //Film has been created
                         console.log('PATCH updating new film: ' + film);
                         res.format({
-                            //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
                             html: function () {
-                                // If it worked, set the header so the address bar doesn't still say /adduser
                                 res.location("films");
-
-                                // And forward to success page
                                 res.redirect("/films");
                             },
-                            //JSON response will show the newly created blob
                             json: function () {
                                 res.json(film);
                             }
